ec655b2e49a3493297231d0d7f7d8fd08a7547ca
jazbedoya <jbedoya628@gmail.com>
2025-12-14 14:38:25 -0300

    Refactor server start and add comments for clarity
    Reorganize server start code and add table creation comment.

--- app.py
+++ app.py
@@ -76,9 +76,15 @@ register_order_routes(app)
 # ---------------------------------------------------
 # INICIO SERVIDOR LOCAL / PRODUCCIÓN
 # ---------------------------------------------------
+# ---------------------------------------------------
+# CREAR TABLAS AUTOMÁTICAMENTE (Render Free)
+# ---------------------------------------------------
 with app.app_context():
     db.create_all()
 
 
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# ---------------------------------------------------
+# INICIO SERVIDOR
+# ---------------------------------------------------
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
